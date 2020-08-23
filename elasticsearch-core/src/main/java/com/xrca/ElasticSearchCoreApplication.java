f21c505af10dca5c7d78b3f45cdb639e5eb51016
xrca <fishsaury@163.com>
2020-08-23 13:27:41 +0800

    添加项目依赖

---
+++ elasticsearch-core/src/main/java/com/xrca/ElasticSearchCoreApplication.java
@@ -0,0 +1,16 @@
+package com.xrca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author xrca
+ * @description 启动类
+ * @date 2020-08-23 12:03
+ */
+@SpringBootApplication
+public class ElasticSearchCoreApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ElasticSearchCoreApplication.class, args);
+    }
+}
